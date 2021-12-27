612fcdcbc36dfadf3fd531ae2a79eefaabd2f091
Exeteres <exeteres@yandex.ru>
2021-12-27 23:11:53 +0700

    fix: Update Kantaiko.Routing

--- src/Kantaiko.Hosting.Lifecycle/Events/IApplicationLifecycleEvent.cs
+++ src/Kantaiko.Hosting.Lifecycle/Events/IApplicationLifecycleEvent.cs
@@ -1,5 +1,3 @@
-using Kantaiko.Routing.Events;
-
 namespace Kantaiko.Hosting.Lifecycle.Events;
 
-public interface IApplicationLifecycleEvent : IEvent { }
+public interface IApplicationLifecycleEvent { }
